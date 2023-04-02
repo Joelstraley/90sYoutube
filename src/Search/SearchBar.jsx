5b405a49e7a09141e4ec3c53c5c6336f1987f160
Joel Straley <joelstraley@gmail.com>
2023-04-02 19:25:34 -0400

    header styling mostly fixed

--- src/Search/SearchBar.jsx
+++ src/Search/SearchBar.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { Button, TextInput } from 'react95'
+import avatar from './../assets/avatar-image.png'
 import './SearchBar.css'
 
 export default function SearchBar({ onFormSubmit }) {
@@ -16,26 +17,32 @@ export default function SearchBar({ onFormSubmit }) {
 		<div className="search-bar">
 			<div className="search-bar__header">
 				<div className="search-bar__logo">
-					<img src="https://win98icons.alexmeub.com/icons/png/monitor_windows.png" />
+					<img
+						className="search-bar__logo--image"
+						alt="Windows-Monitor-Logo"
+						src="https://win98icons.alexmeub.com/icons/png/monitor_windows.png"
+					/>
 					<h3 className="search-bar__title">NetTube</h3>
 				</div>
-				<div className="search-bar__input">
-					<form onSubmit={handleSubmit}>
-						<TextInput
-							id="search-input"
-							type="text"
-							placeholder="Search..."
-							onChange={(e) => {
-								setSearchTerm(e.target.value)
-							}}
-							width={15}
+				<div className="search-bar__input-container">
+					<div className="search-bar__input">
+						<form onSubmit={handleSubmit}>
+							<TextInput
+								id="search-input"
+								type="text"
+								placeholder="Search..."
+								onChange={(e) => {
+									setSearchTerm(e.target.value)
+								}}
+								width={15}
+							/>
+						</form>
+						<img
+							class="search-bar__icon"
+							src="https://win98icons.alexmeub.com/icons/png/search_web-0.png"
+							alt="Windows-Magnifying-Glass-Icon"
 						/>
-					</form>
-					<img
-						class="search-bar__icon"
-						src="https://win98icons.alexmeub.com/icons/png/search_web-0.png"
-						alt="windows magnifying glass icon"
-					/>
+					</div>
 					<div className="search-bar__btns-container">
 						<Button
 							className="search-bar__btns"
@@ -50,6 +57,15 @@ export default function SearchBar({ onFormSubmit }) {
 							Upload Video
 						</Button>
 					</div>
+				</div>
+				<div className="search-bar__avatar">
+					<a href="https://joel-straley-portfolio.netlify.app/">
+						<img
+							src={avatar}
+							alt="Broken-Img-Icon"
+							className="search-bar__avatar-img"
+						/>
+					</a>
 				</div>
 			</div>
 		</div>
